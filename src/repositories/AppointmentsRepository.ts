0e3d65c77170c672f4264ce55d51217b4c32e008
Lucas Engel <lucasengel@gmail.com>
2020-09-02 13:42:35 -0400

    Initial commit

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,38 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+// DTO = Data Transfer Object
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+class ApppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment => {
+      return isEqual(date, appointment.date);
+    });
+
+    return findAppointment || null;
+  }
+}
+
+export default ApppointmentsRepository;
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server started on: http://localhost:3333/');
+});
