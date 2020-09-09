d8ac98022c8407370d38f63467da73bdf307943b
Lucas Engel <lucasengel@gmail.com>
2020-09-09 11:58:00 -0400

    Adds CreateUserService + relationship with apptmts

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,15 +1,35 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
+
+import User from "./Users";
 
 @Entity("appointments")
 class Appointment {
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "provider_id" })
+  provider: User;
+
   @Column()
-  provider: string;
+  provider_id: string;
 
   @Column("time with time zone")
   date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Appointment;
