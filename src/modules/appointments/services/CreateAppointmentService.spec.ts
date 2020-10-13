996cda7551288b184a7039f8f6684424ff1740bc
Lucas Engel <lucasengel@gmail.com>
2020-10-12 20:15:38 -0400

    FEATURE: add conditions to appt booking

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { addDays, setHours } from 'date-fns';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -13,32 +14,74 @@ describe('CreateAppointment', () => {
 
   it('should be able to create a new appointment.', async () => {
     const appointment = await createAppointment.execute({
-      date: new Date(),
-      provider_id: 'non-existing-provider-id',
-      user_id: 'non-existing-user-id',
+      date: addDays(new Date(setHours(Date.now(), 9)), 3),
+      provider_id: 'provider-id',
+      user_id: 'user-id',
     });
 
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('non-existing-provider-id');
+    expect(appointment.provider_id).toBe('provider-id');
   });
 
   it('should NOT be able to create appointments at the same time.', async () => {
     appointmentsRepository = new FakeAppointmentsRepository();
     createAppointment = new CreateAppointmentService(appointmentsRepository);
 
-    const appointmentDate = new Date();
+    const appointmentDate = addDays(new Date(setHours(Date.now(), 9)), 3);
 
     await createAppointment.execute({
       date: appointmentDate,
-      provider_id: 'non-existing-provider-id',
-      user_id: 'non-existing-user-id',
+      provider_id: 'provider-id',
+      user_id: 'user-id',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
-        provider_id: 'non-existing-provider-id',
-        user_id: 'non-existing-user-id',
+        provider_id: 'provider-id',
+        user_id: 'user-id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should NOT be able to create an appointment in the past', async () => {
+    jest
+      .spyOn(Date, 'now')
+      .mockImplementationOnce(() => new Date(2020, 4, 10, 12).getTime());
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 10, 11),
+        provider_id: 'provider-id',
+        user_id: 'user-id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should NOT be able to create an appointment with own user', async () => {
+    await expect(
+      createAppointment.execute({
+        date: addDays(new Date(), 3),
+        provider_id: 'same-id',
+        user_id: 'same-id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should NOT be able to create an appointment outside work hours', async () => {
+    await expect(
+      createAppointment.execute({
+        date: addDays(new Date(setHours(Date.now(), 7)), 3),
+        provider_id: 'provider-id',
+        user_id: 'user-id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+
+    await expect(
+      createAppointment.execute({
+        date: addDays(new Date(setHours(Date.now(), 23)), 3),
+        provider_id: 'provider-id',
+        user_id: 'user-id',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { getHours, isBefore, isWithinInterval, startOfHour } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -24,6 +24,17 @@ class CreateAppointmentService {
     user_id,
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
+    // check if appt is in the past
+    if (isBefore(appointmentDate, Date.now()))
+      throw new AppError("Appointment can't be in the past.");
+
+    // check if appt was booked with own user
+    if (user_id === provider_id)
+      throw new AppError("Appointment can't be booked with own user");
+
+    // check if appt is within work hours
+    if (getHours(date) < 8 || getHours(date) > 17)
+      throw new AppError('Appointment is outside work hours');
 
     const findConflictingAppointment = await this.appointmentsRepository.findByDate(
       appointmentDate,
