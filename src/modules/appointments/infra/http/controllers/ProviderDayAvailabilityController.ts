aa7d357480d892ebebba7c91099b9632d1b08e88
Lucas Engel <lucasengel@gmail.com>
2020-10-12 20:54:59 -0400

    FEATURE: provider day/month controllers and routes

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,25 @@
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+class ProviderDayAvailabilityController {
+  public async list(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { year, month, day } = request.body;
+
+    const listProviderDayAvailability = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const availability = await listProviderDayAvailability.execute({
+      provider_id,
+      year,
+      month,
+      day,
+    });
+
+    return response.json(availability);
+  }
+}
+
+export default ProviderDayAvailabilityController;
---
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -0,0 +1,24 @@
+import ListProviderMonthAvailabilityService from '@modules/appointments/services/ListProviderMonthAvailabilityService';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+class ListProviderMonthAvailabilityController {
+  public async list(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { year, month } = request.body;
+
+    const listProviderMonthAvailability = container.resolve(
+      ListProviderMonthAvailabilityService,
+    );
+
+    const availability = await listProviderMonthAvailability.execute({
+      provider_id,
+      year,
+      month,
+    });
+
+    return response.json(availability);
+  }
+}
+
+export default ListProviderMonthAvailabilityController;
