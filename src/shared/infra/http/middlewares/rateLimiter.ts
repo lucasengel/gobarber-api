40b20d974b9e10c3e9e45401e9f904290a6e1efd
Lucas Engel <lucasengel@gmail.com>
2020-10-15 20:16:52 -0400

    FEATURE: rateLimiter 5 req per sec

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/errors/AppError';
+import { Request, Response, NextFunction } from 'express';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import redis from 'redis';
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASS,
+});
+
+const rateLimiter = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> => {
+  const limiter = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: 'ratelimit',
+    points: 5,
+    duration: 1,
+  });
+
+  try {
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (error) {
+    throw new AppError('Too many request', 429);
+  }
+};
+
+export default rateLimiter;
